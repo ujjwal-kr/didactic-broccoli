4090c32a75d3ea74926af294c42d1101f3c5e73d
ujjwal-kr <katanadeveloper01@gmail.com>
2020-04-04 17:03:05 +0530

    added comonents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TalkComponent } from './talk/talk.component';
+import { VerifyComponent } from './verify/verify.component';
+import { SelectComponent } from './select/select.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '/', component: HomeComponent},
+  {path: '/name', component: VerifyComponent},
+  {path: '/select', component: SelectComponent},
+  {path: '/main', component: TalkComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
